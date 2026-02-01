c246f540e212e1e9eef9072d34b7e456ec196d58
dmitrined <dm.nedioglo@gmail.com>
2026-02-01 16:45:03 +0100

    feat: implement UserRepository and RoleRepository interfaces

--- src/main/java/de/ait/homerent/user/repository/RoleRepository.java
+++ src/main/java/de/ait/homerent/user/repository/RoleRepository.java
@@ -1,4 +1,14 @@
 package de.ait.homerent.user.repository;
 
-public class RoleRepository {
+import de.ait.homerent.user.model.Role;
+import de.ait.homerent.user.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(RoleName name);
 }
