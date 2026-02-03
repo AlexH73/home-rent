68ece58f1689b13fb7a79a80308a2367ff5ea831
TetianaAnufriieva <anufrieva.tetiana@gmail.com>
2026-02-03 11:54:05 +0100

    feat (IssueReport entity and repository creation)

---
+++ src/main/java/de/ait/homerent/issue/repository/IssueReportRepository.java
@@ -0,0 +1,24 @@
+package de.ait.homerent.issue.repository;
+
+import de.ait.homerent.issue.model.IssueReport;
+import de.ait.homerent.issue.model.IssueStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * ----------------------------------------------------------------------------
+ * Author  : Tetiana Anufriieva
+ * Created : 03.02.2026
+ * Project : home-rent
+ * ----------------------------------------------------------------------------
+ */
+public interface IssueReportRepository extends JpaRepository<IssueReport, Long> {
+
+    List<IssueReport> findByStatus (IssueStatus status);
+
+    List<IssueReport> findByBookingId(Long bookingId);
+
+    List<IssueReport> findByReportedById(Long userId);
+
+}
