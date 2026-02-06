863ff1baa87815716c6a556e6c737fb8aee07f3d
Alexander Hermann <ewebotah@gmail.com>
2026-02-06 15:46:29 +0100

    feat: log Swagger UI URL on startup
    - Added a log message displaying the URL for Swagger UI during application startup to improve developer accessibility.

--- src/main/java/de/ait/homerent/HomeRentApplication.java
+++ src/main/java/de/ait/homerent/HomeRentApplication.java
@@ -12,6 +12,7 @@ public class HomeRentApplication {
         System.out.println("###########################################");
         System.out.println("HomeRent Application started successfully!");
         System.out.println("###########################################");
+        System.out.println("Swagger UI available at: http://localhost:8080/swagger-ui.html");
         System.out.println("H2 Console available at: http://localhost:8080/h2-console");
         System.out.println("JDBC URL: jdbc:h2:mem:homerent_dev");
         System.out.println("Username: sa");
