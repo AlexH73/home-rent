1006017cd7ea52da5410e7ef676bbb3006ceb03b
TetianaAnufriieva <anufrieva.tetiana@gmail.com>
2026-02-14 16:44:08 +0100

    feat(email): сreating Thymeleaf templates for email notifications

---
+++ src/main/java/de/ait/homerent/booking/dto/RentalFinishedEmailRequest.java
@@ -0,0 +1,42 @@
+package de.ait.homerent.booking.dto;
+
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+/**
+ * ----------------------------------------------------------------------------
+ * Author  : Tetiana Anufriieva
+ * Created : 13.02.2026
+ * Project : home-rent
+ * ----------------------------------------------------------------------------
+ */
+
+
+@Getter
+@Setter
+public class RentalFinishedEmailRequest {
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String propertyAddress;
+
+    @NotNull
+    private LocalDate startDate;
+
+    @NotNull
+    private LocalDate endDate;
+
+    @Positive
+    private Integer totalPrice;
+}
+
