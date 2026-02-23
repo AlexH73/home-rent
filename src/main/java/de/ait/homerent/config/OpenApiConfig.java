d431454e83b16db1877f59e0a17e58bfb8d6a6af
Alexander Hermann <ewebotah@gmail.com>
2026-02-24 00:29:23 +0100

    add: define global OpenAPI security configuration and adjust logging level
    - Added `customOpenAPI` method defining global basicAuth requirement in `OpenApiConfig`.
    - Configured HTTP basic authentication scheme in components.
    - Adjusted logging level for `org.springframework.security` to ERROR in `application.properties`.

--- src/main/java/de/ait/homerent/config/OpenApiConfig.java
+++ src/main/java/de/ait/homerent/config/OpenApiConfig.java
@@ -8,7 +8,11 @@ import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 /**
  * ----------------------------------------------------------------------------
  * Author  : Alexander Hermann
@@ -73,4 +77,21 @@ import org.springframework.context.annotation.Configuration;
         }
 )
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("HomeRent API")
+                        .version("1.0.0")
+                        .description("HomeRent API Documentation"))
+                // Глобальное требование безопасности: все эндпоинты требуют basicAuth
+                .addSecurityItem(new SecurityRequirement().addList("basicAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("basicAuth",
+                                new io.swagger.v3.oas.models.security.SecurityScheme()
+                                        .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                                        .scheme("basic")
+                                        .description("Basic authentication using username and password")));
+    }
 }
