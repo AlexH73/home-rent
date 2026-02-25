e8744f8668511e8dde1051f41a2038b6723f098f
Alexander Hermann <ewebotah@gmail.com>
2026-02-25 06:08:14 +0100

    add: enable test profile for HomeRentApplication tests

--- src/test/java/de/ait/homerent/HomeRentApplicationTests.java
+++ src/test/java/de/ait/homerent/HomeRentApplicationTests.java
@@ -2,7 +2,10 @@ package de.ait.homerent;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+
+@ActiveProfiles("test")
 @SpringBootTest(classes = HomeRentApplication.class)
 class HomeRentApplicationTests {
     @Test void contextLoads() {}
